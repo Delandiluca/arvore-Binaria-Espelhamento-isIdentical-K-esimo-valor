cb75ed6d35bdb367628a68b9fceb37e1d2f1c76e
Delandiluca <121880539+Delandiluca@users.noreply.github.com>
2023-10-09 18:10:42 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Arvore-Atividade-II-estrutura-de-dados-bsi-2023-12/main.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+typedef struct node Node;
+
+Node *newNode(int data);
+void createTree(Node **tree);
+void insertBST(Node **node, int data);
+int treeIsEmpty(Node *node);
+int isIdentical(Node *one, Node *two);
+void invertTree(Node *root);
+void preOrder(Node *root);
+int returnKLargest(Node* root, int k);
+
+int main(void) {
+  Node *rootOne;
+  Node *rootTwo;
+  createTree(&rootOne);
+  createTree(&rootTwo);
+
+  insertBST(&rootOne, 10);
+  insertBST(&rootOne, 5);
+  insertBST(&rootOne, 15);
+  insertBST(&rootOne, 2);
+  insertBST(&rootOne, 6);
+  insertBST(&rootOne, 12);
+  insertBST(&rootOne, 17);
+
+  insertBST(&rootTwo, 5);
+  insertBST(&rootTwo, 1);
+  insertBST(&rootTwo, 4);
+
+  // if (isIdentical(rootOne, rootTwo)) {
+  // puts("\nSÃO IGUAIS");
+  //} else {
+  // puts("\nNÃO SÃO IGUAIS");
+  //}
+
+  //invertTree(rootOne);
+  //preOrder(rootOne);
+  int data = returnKLargest(rootOne, 2);
+  
+
+  return 0;
+}
+
+void createTree(Node **tree) { *tree = NULL; }
+
+int treeIsEmpty(Node *node) { return node == NULL; }
+
+Node *newNode(int data) {
+  // Aloca memória para newNode
+  Node *node = (Node *)malloc(sizeof(Node));
+  // Atribui o dado a newNode
+  node->data = data;
+  // Atribui NULL a left e right
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
+}
+
+void insertBST(Node **node, int data) {
+  if (treeIsEmpty(*node)) {
+    *node = newNode(data);
+  } else {
+    if (data < (*node)->data) { // ESQUERDA SE FOR MENOR
+      insertBST(&(*node)->left, data);
+    }
+    if (data > (*node)->data) { // DIREITA SE FOR MAIOR
+      insertBST(&(*node)->right, data);
+    }
+  }
+}
+
+int isIdentical(Node *one, Node *two) {
+  if ((one == NULL) && (two == NULL)) {
+    return 1;
+  }
+
+  return ((one != NULL) && (two != NULL)) && ((one->data == two->data)) &&
+         isIdentical(one->left, two->left) &&
+         isIdentical(one->right, two->right);
+}
+
+int returnKLargest(Node* root, int k) {
+    int count = 0;
+    if (root == NULL) {
+        return 0;
+    }
+    int result = returnKLargest(root->right, k);
+    if (result != 0) {
+        return result;
+    }
+    count++;
+    if (count == k) {
+        return root->data;
+    }
+    return returnKLargest(root->left, k);
+}
+
+void invertTree(Node *root) {
+  if (root == NULL) {
+    return;
+  }
+
+  Node *temp = root->left;
+  root->left = root->right;
+  root->right = temp;
+
+  invertTree(root->left);
+  invertTree(root->right);
+}
+
+void preOrder(Node *root) {
+  if (root != NULL) {
+    printf("\n%i", root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+  }
+}
---
+++ .lesson/instructions.md
@@ -0,0 +1,34 @@
+# Instruções
+
+### 1 Determine se duas árvores binárias são idênticas ou não
+
+Exemplo:  
+```
+entrada 1:
+
+   4          1 
+  / \          \ 
+ 1   5          4 
+                 \ 
+								  5 
+
+Saída: Não
+
+Entrada 2:
+
+	 5            5
+  /            /
+ 1            1
+  \            \
+	 4            4
+
+Saída: Sim
+```
+
+A tarefa consiste em definir uma função que receba duas árvores como parâmetro e retorne verdadeiro ou falso como resultado da comparação
+
+### 2 Dada uma árvore de pesquisa binária BST. Sua tarefa é implementar a função que retornará o K-ésimo maior elemento sem fazer nenhuma modificação na Árvore de Pesquisa Binária
+
+### 3 Desenvolva uma função que faça o espelhamento de árvore binária
+
+
